d71f7e4994b6e9246029f36a35b030ea6d56c9a9
Hector <a286054@uach.mx>
2020-12-06 19:30:27 -0700

    Se agrego en /about un json

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ const path = require('path')
 app.get("",(request,response)=>{
   return response.sendFile(path.join(__dirname+"./../assets/html/index.html"))
 })
+app.get("/about",(request,response)=>{
+  return response.json({
+  servidor: "Mi web server",
+  equipo: [{name:"Hector", lastName:"Silvadoray Ornelas"}]
+  })
+})
 
 app.listen(4000, () => {
   console.log("Servidor iniciado");
