16c4ea41216223a4109922b2cd81839061e008a9
Hector <a286054@uach.mx>
2020-12-08 23:11:08 -0700

    Se agregó todo el entorno gráfico de html

--- server/app.js
+++ server/app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-app.get("",(request,response)=>{
-  return response.sendFile(path.join(__dirname+"./../assets/html/index.html"))
+//middleware para recibir archivos
+app.use(express.static(__dirname+"./../assets"))
+//ruta de index
+app.get("/",(request,response)=>{
+
+  return response.sendFile(path.join(__dirname+"./../assets/index.html"))
 })
+//ruta de curriculum
+app.get("/curriculum",(request,response)=>{
+
+  return response.sendFile(path.join(__dirname+"./../assets/curriculum.html"))
+})
+//ruta de about de json
 app.get("/about",(request,response)=>{
   return response.json({
   servidor: "Mi web server",
   equipo: [{name:"Hector", lastName:"Silvadoray Ornelas"}]
   })
 })
-
+//Puerto de uso
 app.listen(4000, () => {
   console.log("Servidor iniciado");
 })
